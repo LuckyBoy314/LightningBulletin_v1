b5bddd85c5d3f6f3e3f167afb5fb856d4e6e8d55
LuckyBoy314 <zhufeng314@163.com>
2016-07-28 18:25:22 +0800

    优化SQL查询，还没有实现最好的方法

--- StatisticsProcess.py
+++ StatisticsProcess.py
@@ -22,9 +22,9 @@
 
 # import arcpy
 #
-# arcpy.CreatePersonalGDB_management("/", "GDB.mdb")
+# arcpy.CreatePersonalGDB_management(''.join([cwd,'/data']), "GDB.mdb")
 # data = u'D:/bulletinTemp/2015年/data2015年.shp'
-# arcpy.FeatureClassToGeodatabase_conversion(data,'/GDB.mdb')
+# arcpy.FeatureClassToGeodatabase_conversion(data,''.join([cwd,'/data','/GDB.mdb']))
 
 import pyodbc
 import win32com.client
@@ -58,7 +58,7 @@ results = {}
 for row in cursor.execute(sql):
     results[row[1]] = row[0]  # 以 Region：num建立字典，方便下面赋值
 
-sheet = workbook.Worksheets(u'省地区')
+sheet = workbook.Worksheets(u'省分区')
 for row in xrange(2, 13):
     sheet.Cells(row, 1).Value = results[sheet.Cells(row, 2).Value]
 
@@ -81,47 +81,47 @@ for row in xrange(3, 9):
 
 # ************分月统计 月地闪次数和月平均强度(负闪)**************
 sql = """
-SELECT count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,1 as 月份
+SELECT count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,1 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/1/1# And Date_< #YEAR/2/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,2 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/1/1# AND Date_< #YEAR/2/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,2 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/2/1# And Date_< #YEAR/3/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,3 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/2/1# AND Date_< #YEAR/3/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,3 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/3/1# And Date_< #YEAR/4/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,4 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/3/1# AND Date_< #YEAR/4/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,4 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/4/1# And Date_< #YEAR/5/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,5 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/4/1# AND Date_< #YEAR/5/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,5 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/5/1# And Date_< #YEAR/6/1#AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,6 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/5/1# AND Date_< #YEAR/6/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,6 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/6/1# And Date_< #YEAR/7/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,7 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/6/1# AND Date_< #YEAR/7/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,7 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/7/1# And Date_< #YEAR/8/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,8 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/7/1# AND Date_< #YEAR/8/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,8 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/8/1# And Date_< #YEAR/9/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,9 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/8/1# AND Date_< #YEAR/9/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,9 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/9/1# And Date_< #YEAR/10/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,10 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/9/1# AND Date_< #YEAR/10/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,10 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/10/1# And Date_< #YEAR/11/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,11 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/10/1# AND Date_< #YEAR/11/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,11 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/11/1# And Date_< #YEAR/12/1# AND Region='绍兴市' and Intensity<0)
-union SELECT  count(*) as 负闪次数, -sum(Intensity)/count(*) as 平均强度 ,12 as 月份
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/11/1# AND Date_< #YEAR/12/1#
+union SELECT  count(*) AS 负闪次数, -sum(Intensity)/count(*) AS 平均强度 ,12 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/12/1# And Date_<=#YEAR/12/31# AND Region='绍兴市' and Intensity<0)
+WHERE Region='绍兴市' AND Intensity<0 AND Date_>=#YEAR/12/1# AND Date_<=#YEAR/12/31#
 ORDER BY 月份
 """.replace('QUERY_TABLE',data_table).replace('YEAR',str(year))
 
-sheet1 = workbook.Worksheets(u'月份')
-sheet2 = workbook.Worksheets(u'地闪强度波动')
+sheet1 = workbook.Worksheets(u'分月地闪次数统计')
+sheet2 = workbook.Worksheets(u'分月地闪强度统计')
 i =1#行号
 for row in cursor.execute(sql):
     i+=1
@@ -131,42 +131,42 @@ for row in cursor.execute(sql):
 
 # ************分月统计 月地闪次数和月平均强度(正闪)**************
 sql = """
-SELECT count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,1 as 月份
+SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,1 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/1/1# And Date_< #YEAR/2/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,2 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/1/1# AND Date_< #YEAR/2/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,2 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/2/1# And Date_< #YEAR/3/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,3 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/2/1# AND Date_< #YEAR/3/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,3 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/3/1# And Date_< #YEAR/4/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,4 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/3/1# AND Date_< #YEAR/4/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,4 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/4/1# And Date_< #YEAR/5/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,5 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/4/1# AND Date_< #YEAR/5/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,5 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/5/1# And Date_< #YEAR/6/1#AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,6 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/5/1# AND Date_< #YEAR/6/1#AND Region='绍兴市'
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,6 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/6/1# And Date_< #YEAR/7/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,7 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/6/1# AND Date_< #YEAR/7/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,7 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/7/1# And Date_< #YEAR/8/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,8 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/7/1# AND Date_< #YEAR/8/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,8 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/8/1# And Date_< #YEAR/9/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,9 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/8/1# AND Date_< #YEAR/9/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,9 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/9/1# And Date_< #YEAR/10/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,10 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/9/1# AND Date_< #YEAR/10/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,10 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/10/1# And Date_< #YEAR/11/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,11 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/10/1# AND Date_< #YEAR/11/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,11 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/11/1# And Date_< #YEAR/12/1# AND Region='绍兴市' and Intensity>0)
-union SELECT  count(*) as 正闪次数, sum(Intensity)/count(*) as 平均强度 ,12 as 月份
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/11/1# AND Date_< #YEAR/12/1#
+union SELECT count(*) AS 正闪次数, sum(Intensity)/count(*) AS 平均强度 ,12 AS 月份
 FROM QUERY_TABLE
-WHERE (Date_>=#YEAR/12/1# And Date_<=#YEAR/12/31# AND Region='绍兴市' and Intensity>0)
+WHERE Region='绍兴市' AND Intensity>0 AND Date_>=#YEAR/12/1# AND Date_<=#YEAR/12/31#
 ORDER BY 月份
 """.replace('QUERY_TABLE',data_table).replace('YEAR',str(year))
 
@@ -175,6 +175,169 @@ for row in cursor.execute(sql):
     i+=1
     sheet1.Cells(i,2).Value = row[0] #正闪次数
     sheet2.Cells(i,3).Value = row[1] if row[1] is not None else 0 #正闪强度
+
+#**********负闪强度分布**************
+sql = """
+SELECT count(*) AS 负闪次数,0 AS 左边界,5 AS 右边界
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=0 AND Abs(Intensity)<5
+union SELECT count(*) AS 负闪次数,5,10
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=5 AND Abs(Intensity)<10
+union SELECT count(*) AS 负闪次数,10,15
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=10 AND Abs(Intensity)<15
+union SELECT count(*) AS 负闪次数,15,20
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=15 AND Abs(Intensity)<20
+union SELECT count(*) AS 负闪次数,20,25
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=20 AND Abs(Intensity)<25
+union SELECT count(*) AS 负闪次数,25,30
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=25 AND Abs(Intensity)<30
+union SELECT count(*) AS 负闪次数,30,35
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=30 AND Abs(Intensity)<35
+union SELECT count(*) AS 负闪次数,35,40
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=35 AND Abs(Intensity)<40
+union SELECT count(*) AS 负闪次数,40,45
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=40 AND Abs(Intensity)<45
+union SELECT count(*) AS 负闪次数,45,50
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=45 AND Abs(Intensity)<50
+union SELECT count(*) AS 负闪次数,50,55
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=50 AND Abs(Intensity)<55
+union SELECT count(*) AS 负闪次数,55,60
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=55 AND Abs(Intensity)<60
+union SELECT count(*) AS 负闪次数,60,65
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=60 AND Abs(Intensity)<65
+union SELECT count(*) AS 负闪次数,65,70
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=65 AND Abs(Intensity)<70
+union SELECT count(*) AS 负闪次数,70,75
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=70 AND Abs(Intensity)<75
+union SELECT count(*) AS 负闪次数,75,80
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=75 AND Abs(Intensity)<80
+union SELECT count(*) AS 负闪次数,80,85
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=80 AND Abs(Intensity)<85
+union SELECT count(*) AS 负闪次数,85,90
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=85 AND Abs(Intensity)<90
+union SELECT count(*) AS 负闪次数,90,95
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=90 AND Abs(Intensity)<95
+union SELECT count(*) AS 负闪次数,95,100
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=95 AND Abs(Intensity)<100
+union SELECT count(*) AS 负闪次数,100,150
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=100 AND Abs(Intensity)<150
+union SELECT count(*) AS 负闪次数,150,200
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=150 AND Abs(Intensity)<200
+union SELECT count(*) AS 负闪次数,200,250
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=200 AND Abs(Intensity)<250
+union SELECT count(*) AS 负闪次数,250,300
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=250 AND Abs(Intensity)<300
+UNION SELECT count(*) AS 负闪次数,300,1000
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity<0 AND Abs(Intensity)>=300
+ORDER BY 左边界
+""".replace("QUERY_TABLE",data_table)
+
+
+
+#***********正闪强度分布************
+sql = """
+SELECT count(*) AS 正闪次数,0 AS 左边界,5 AS 右边界
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=0 AND Intensity<5
+union SELECT count(*) AS 正闪次数,5,10
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=5 AND Intensity<10
+union SELECT count(*) AS 正闪次数,10,15
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=10 AND Intensity<15
+union SELECT count(*) AS 正闪次数,15,20
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=15 AND Intensity<20
+union SELECT count(*) AS 正闪次数,20,25
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=20 AND Intensity<25
+union SELECT count(*) AS 正闪次数,25,30
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=25 AND Intensity<30
+union SELECT count(*) AS 正闪次数,30,35
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=30 AND Intensity<35
+union SELECT count(*) AS 正闪次数,35,40
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=35 AND Intensity<40
+union SELECT count(*) AS 正闪次数,40,45
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=40 AND Intensity<45
+union SELECT count(*) AS 正闪次数,45,50
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=45 AND Intensity<50
+union SELECT count(*) AS 正闪次数,50,55
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=50 AND Intensity<55
+union SELECT count(*) AS 正闪次数,55,60
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=55 AND Intensity<60
+union SELECT count(*) AS 正闪次数,60,65
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=60 AND Intensity<65
+union SELECT count(*) AS 正闪次数,65,70
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=65 AND Intensity<70
+union SELECT count(*) AS 正闪次数,70,75
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=70 AND Intensity<75
+union SELECT count(*) AS 正闪次数,75,80
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=75 AND Intensity<80
+union SELECT count(*) AS 正闪次数,80,85
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=80 AND Intensity<85
+union SELECT count(*) AS 正闪次数,85,90
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=85 AND Intensity<90
+union SELECT count(*) AS 正闪次数,90,95
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=90 AND Intensity<95
+union SELECT count(*) AS 正闪次数,95,100
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=95 AND Intensity<100
+union SELECT count(*) AS 正闪次数,100,150
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=100 AND Intensity<150
+union SELECT count(*) AS 正闪次数,150,200
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=150 AND Intensity<200
+union SELECT count(*) AS 正闪次数,200,250
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=200 AND Intensity<250
+union SELECT count(*) AS 正闪次数,250,300
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=250 AND Intensity<300
+UNION SELECT count(*) AS 正闪次数,300,1000
+FROM QUERY_TABLE
+WHERE Region='绍兴市' AND Intensity>=300
+ORDER BY 左边界
+""".replace("QUERY_TABLE",data_table)
+
 
 workbook.Save()  # 保存EXCEL工作薄
 workbook.Close()  # 关闭工作薄文件
